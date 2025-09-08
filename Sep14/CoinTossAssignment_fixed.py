578f525307b8517810d9e5607a454ea305cfd865
rvrbetastudent <rsvrbeta@students.nic.edu>
2025-09-08 09:24:01 -0700

    made the bugged assignment into a proper functioning .py file. before it would try to compare a int to a string, always resulting in a false answer.

--- Sep14/CoinTossAssignment_fixed.py
+++ Sep14/CoinTossAssignment_fixed.py
@@ -4,12 +4,16 @@ while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
     guess = input()
 toss = random.randint(0, 1)  # 0 is tails, 1 is heads
-if toss == guess:
+# Map guess to int
+guess_int = 1 if guess =='heads' else 0
+
+if toss == guess_int:
     print('You got it!')
 else:
     print('Nope! Guess again!')
     guess = input()
-    if toss == guess:
+    guess_int =1 if guess == 'heads' else 0
+    if toss == guess_int:
         print('You got it!')
     else:
         print('Nope. You are really bad at this game.')
