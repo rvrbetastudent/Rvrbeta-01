3c3cc2065bba4072ca4e9f15d16a9be4e3df41ea
rvrbetastudent <rsvrbeta@students.nic.edu>
2025-09-10 09:15:24 -0700

    fixing an infinite loop in the Collatz Sequence program I made last week.

--- Sep7/TheCollatzSequence.py
+++ Sep7/TheCollatzSequence.py
@@ -2,6 +2,9 @@ try:
     n = int(input("Enter number: "))
     print(n, end=' ')
     while n != 1:
+        if n == 0:
+            print("is not allowed. Please enter a different number.")
+            break
         n = n // 2 if n % 2 == 0 else 3 * n + 1
         print(n, end=' ')
     print()
